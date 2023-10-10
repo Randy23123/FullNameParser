e05b04164ba6ddedc05253d56f01a508d6bd07fd
Randy23123 <rlopez@appdev.yearup.org>
2023-10-10 14:08:55 -0400

    done with code

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameParser {
+    public static void main(String[] args) {
+        Scanner scannner = new Scanner(System.in);{
+            String fullName = "", firstName = "", middleName = "", lastName = "";
+            System.out.print("Please enter your full name: ");
+            fullName = scannner.nextLine().trim();
+            firstName = fullName.substring(0, fullName.indexOf(" "));
+            fullName = fullName.substring(fullName.indexOf(" ") + 1);
+            if(fullName.contains(" ")) {
+                middleName = fullName.substring(0, fullName.indexOf(" "));
+                fullName = fullName.substring(fullName.indexOf(" ") + 1);
+                lastName = fullName;
+            }
+            else{
+                lastName = fullName;
+            }
+            System.out.println("First name: " + firstName + "\nMiddle name: " + middleName + "\nLast name: " + lastName);
+
+        }
+    }
 }
